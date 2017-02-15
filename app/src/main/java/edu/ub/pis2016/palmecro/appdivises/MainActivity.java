6f3e085433f68acb3a60522bd4a0ec1a0117f71b
yockiehub <pabloalmecija@gmail.com>
2017-02-15 13:48:12 +0100

    - Added app icon. - Methods encapsulation to avoid code duplicity.

--- app/src/main/java/edu/ub/pis2016/palmecro/appdivises/MainActivity.java
+++ app/src/main/java/edu/ub/pis2016/palmecro/appdivises/MainActivity.java
@@ -2,6 +2,8 @@ package edu.ub.pis2016.palmecro.appdivises;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,9 +11,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText amountText;
-    private EditText changeText;
-    private EditText taxText;
+    private EditText eurosEt;
+    private EditText dollarsEt;
+    private EditText changeEd;
+    private EditText taxEt;
 
     private Button eurToDolButton;
     private Button dolToEurButton;
@@ -21,40 +24,121 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        amountText = (EditText)findViewById(R.id.amount);
-        changeText = (EditText)findViewById(R.id.change);
-        taxText = (EditText)findViewById(R.id.tax);
+        eurosEt = (EditText)findViewById(R.id.euros);
+        dollarsEt = (EditText)findViewById(R.id.dollars);
+        changeEd = (EditText)findViewById(R.id.change);
+        taxEt = (EditText)findViewById(R.id.tax);
 
         eurToDolButton = (Button)findViewById(R.id.eurToDol);
+        dolToEurButton = (Button)findViewById(R.id.dolToEur);
+
+        eurosEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                textChange(s, eurosEt);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
         eurToDolButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                double val;
-                String str;
-                if (!amountText.getText().toString().isEmpty() && (!changeText.getText().toString().isEmpty() &&
-                        !changeText.getText().toString().equals('0'))){
-                    val = Double.parseDouble(amountText.getText().toString());
-                    val = val * Double.parseDouble(changeText.getText().toString());
-                    if (!taxText.getText().toString().isEmpty()){
-                        double taxRate = Double.parseDouble(taxText.getText().toString());
-                        val = val - (taxRate/100 * val);
-                    }
-                    str = String.valueOf(val);
-                    amountText.setText(str);
-                }else if (amountText.getText().toString().isEmpty()){
-                    Toast.makeText(MainActivity.this, "Enter a correct amount", Toast.LENGTH_SHORT).show();
+                buttonClick(eurosEt, dollarsEt, taxEt, changeEd);
+            }
+        });
+        dollarsEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-                }else if (changeText.getText().toString().isEmpty() || changeText.getText().toString().equals('0')){
-                    Toast.makeText(MainActivity.this, "Enter a correct change rate", Toast.LENGTH_SHORT).show();
-                }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                textChange(s, dollarsEt);
+            }
 
-
-                /*if (){
-
-                }*/
+            @Override
+            public void afterTextChanged(Editable s) {
             }
         });
 
+        dolToEurButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonClick(dollarsEt, eurosEt, taxEt, changeEd);
+            }
+        });
 
+        taxEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                textChange(s, taxEt);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        changeEd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                textChange(s, changeEd);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+/**
+ * Encapsulation of common methods
+ */
+
+    private void textChange(CharSequence s, EditText eT){
+        if (s.toString().equals(".")){
+            eT.setText("0.");
+            eT.setSelection(eT.getText().length());
+        }
+
+        if (s.toString().equals("00")){
+            eT.setText("0");
+            eT.setSelection(eT.getText().length());
+        }
+    }
+
+    private void buttonClick(EditText currencyIn, EditText currencyOut, EditText tax, EditText change){
+        double val;
+        String str;
+        if (!currencyIn.getText().toString().isEmpty() && (!change.getText().toString().isEmpty())){
+            val = Double.parseDouble(currencyIn.getText().toString());
+            val = val * Double.parseDouble(change.getText().toString());
+            if (!tax.getText().toString().isEmpty()){
+                double taxRate = Double.parseDouble(tax.getText().toString());
+                val = val - (taxRate/100 * val);
+            }
+            //str = String.valueOf(val);
+            str = String.format("%.2f", val);
+            currencyOut.setText(str);
+        }else if (currencyIn.getText().toString().isEmpty()){
+            Toast.makeText(MainActivity.this, "Enter a correct amount", Toast.LENGTH_SHORT).show();
+
+        }else if (change.getText().toString().isEmpty() || change.getText().toString().equals("0")){
+            Toast.makeText(MainActivity.this, "Enter a correct change rate", Toast.LENGTH_SHORT).show();
+        }
     }
 }
